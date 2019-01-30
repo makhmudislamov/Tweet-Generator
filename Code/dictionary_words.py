8db0ed89adc6dbcfbdd9183d9a39b8c329137085
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-30 15:58:51 -0800

    putting sample words in an array

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,3 +1,16 @@
-f = open('sample_words.txt', 'r')
-for line in f:
-    print(line)
+import random
+import sys
+
+def read_doc():
+    sample = []
+    # word_num = int(sys.argv[1:])
+    f = open('sample_words.txt', 'r')
+    # loop thourhg the lines of doc
+    # print random x amount of words from the list
+    # x amount = user input number
+
+    for line in f:
+        sample.append(line)
+    print(sample)
+
+read_doc()
