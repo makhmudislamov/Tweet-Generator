4f79a295e5a864bd3737b1afb8c4679f173067c6
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-01-30 16:39:10 -0800

    reading random words from word file

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -4,25 +4,12 @@ import sys
 def read_doc():
     sample = ''
     word_num = int(sys.argv[1])
-    the_file = open('sample_words.txt', 'r')
-    # loop thourhg the lines of doc
-    # print random x amount of words from the list
-    # x amount = user input number
-
+    the_file = open('/usr/share/dict/words', 'r')
+  
     for line in the_file:
         sample += str(line)
-    x = sample.split()
-    print(' '.join(random.choices(x, k=word_num)))
-    
-    
+    listed_sample = sample.split()
+    print(' '.join(random.choices(listed_sample, k=word_num)))
 
 read_doc()
 
-
-# str = 'book pen paper pencil'
-# x = str.split()
-# print(x)
-# y = len(x)
-
-# z = random.randrange(-1, y)
-# print(x[z])
