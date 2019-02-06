4cba31e16930b20fcec17c252dc2e67be9e44182
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-05 16:59:08 -0800

    printing keys (words) of a histogram

--- Code/stoc_sample.py
+++ Code/stoc_sample.py
@@ -1,12 +1,22 @@
-import histogram
+# import histogram
 import sys
-from random import randint
+import random
 
 # command line input >> file name
 cl_input = str(sys.argv[1])
-def rand_word(histogram):
+def rand_word():
     """
     This function returns a random word from the histogram
     """
-    file = open(cl_input, 'r')
-    
+    source_file = open(cl_input, 'r')
+    for key in source_file:
+        print(key)
+
+rand_word()
+
+
+
+# How to print dictionary:
+# source_file = {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1}
+#     for key in source_file:
+#         print(key)
