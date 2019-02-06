3099cf73f77b65b310a2730874ad1d4a23b08c59
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-05 23:05:10 -0800

    finalized list of list works

--- Code/different_histograms.py
+++ Code/different_histograms.py
@@ -1,7 +1,6 @@
-import file_cleaner
-
+import sys
 word_list = ['one', 'fish', 'two', 'two', 'fish', 'red', 'fish', 'blue', 'fish']
-
+cl_input = str(sys.argv[1])
 def histogram_dict(word_list):
     """
     Input >>> array of strings
@@ -40,6 +39,36 @@ def histogram_tuple(word_list):
     print(hist_tuple)
 
 
+def histogram_list(word_list):
+    """
+    Input >>> array of strings
+    Output >>> histogram in a list of list format
+    """
+    hist_list = []
+
+    for word in word_list:
+        found = False                       
+        for value in hist_list:
+            if word in value[0]:
+                found = True
+                value[1] += 1
+
+        if not found:
+            hist_list.append([word, 1])
+
+    print(hist_list)
+
+def format_choice(cl_input):
+    """
+    This is a helper function. Gives a user an option to choose an output format
+    """
+    if cl_input == 'dict':
+        histogram_dict(word_list)
+    elif cl_input == 'tuple':
+        histogram_tuple(word_list)
+    elif cl_input == 'list':
+        histogram_list(word_list)
 
 
-histogram_tuple(word_list)
+if __name__ == '__main__':
+    format_choice(cl_input)
