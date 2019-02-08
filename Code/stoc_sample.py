4787539770cfd874877b0a3f1c75e520f0c89e0e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-07 21:22:35 -0800

    deleted unnecessary comments

--- Code/stoc_sample.py
+++ Code/stoc_sample.py
@@ -57,35 +57,8 @@ if __name__ == "__main__":
     # input_words = list(sys.argv[1:])
     # rand_word()
     histogram = {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1}
-    print(test_iteration(histogram, 10000))    
+    print(test_iteration(histogram, 10000))  
     print(datetime.now()-start_time)
-
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-# import histogram_draft
-# import sys
-# import random
-
-
-# def rand_word(histogram):
-#     """
-#     This function returns a random word from the histogram
-#     """
-#     source_file = open(cl_input, 'r')
-#     for key in source_file:
-#         print(key)
-
-
-
-# if __name__ == "__main__":
-#     # command line input >> file name
-#     cl_input = str(sys.argv[1])
-#     rand_word(histogram_draft)
-    
-# # How to print dictionary:
-# # source_file = {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1}
-# #     for key in source_file:
-# #         print(key)
 
 
 
