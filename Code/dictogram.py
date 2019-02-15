fb3f743acd76eca4437ad37bdb3faedce99d8a0e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-14 17:08:17 -0800

    implemented add_count()

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -19,12 +19,18 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
-       
+        
+        self.tokens += count
+
+        if word in self:
+            self[word] += count
+        else:
+            self.types += 1
+            self[word] = count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count    
+         
         return self[word] if word in self else 0
         
 
