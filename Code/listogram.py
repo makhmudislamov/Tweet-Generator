310476e134f5c9b2c470726565ab84c3c3208b83
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-19 11:57:38 -0800

    another version of frequency()

--- Code/listogram.py
+++ Code/listogram.py
@@ -32,7 +32,11 @@ class Listogram(list):
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         index = self._index(word)
-        return self[index][1] if index is not None else 0
+        # return self[index][1] if index is not None else 0
+        if index is not None:
+            self[index] = (word, self[index][1])
+        else:
+            return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
