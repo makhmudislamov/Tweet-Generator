75b11c39383eed42019f91bb86e2a83a6329e661
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-19 20:58:17 -0800

    created file and wrote pseudocode

---
+++ Code/markov_chain.py
@@ -0,0 +1,14 @@
+import dictogram
+import listogram
+
+def markov_chain_builder(histogram):
+    # should have and empty dict chain_hist = {}
+    # should import histogram
+    # go through the histogram and fill chain_hist = {}
+    #  each new word in the chain_hist will have nested dictionary 
+    # of potential pairs
+    # return chain_hist
+    pass
+
+if __name__ == '__main__':
+    pass
