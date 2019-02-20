2dc1f0a2888a090f048f443fcf7b255de3245c5e
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-20 11:13:08 -0800

    implemented find()

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -93,8 +93,12 @@ class LinkedList(object):
         """Return an item from this linked list satisfying the given quality.
         TODO: Best case running time: O(???) Why and under what conditions?
         TODO: Worst case running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all nodes to find item where quality(item) is True
-        # TODO: Check if node's data satisfies given quality function
+       node = self.head
+       while (node != None):
+           if quality(node.data):
+               return node.data
+            node = node.next
+        return None
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
