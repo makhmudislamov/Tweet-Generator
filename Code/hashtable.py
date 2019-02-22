951ce1c171f0aa345ea58aa4882e7a76e2d897fa
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-21 18:58:41 -0800

    finished hashtable

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -86,19 +86,24 @@ class HashTable(object):
     def set(self, key, value):
         """Insert or update the given key with its associated value.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, update value associated with given key
-        # TODO: Otherwise, insert given key-value entry into bucket
+       
+        key_index = self._bucket_index(key)
+        bucket = self.buckets[key_index]
+        if self.contains(key):
+            self.delete(key)
+        bucket.prepend((key, value))
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, delete entry associated with given key
-        # TODO: Otherwise, raise error to tell user delete failed
-        # Hint: raise KeyError('Key not found: {}'.format(key))
+ 
+        key_index = self._bucket_index(key)
+        bucket = self.buckets[key_index]
+        item = bucket.find(lambda data: data[0] == key)
+        if item:
+            bucket.delete(item)
+        else:
+            raise KeyError('Key not found: {}'.format(key))
 
 
 def test_hash_table():
