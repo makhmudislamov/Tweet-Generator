27d6cb037409d7959f0844a7b07a2aee0bd62776
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-21 18:52:16 -0800

    implemented get()

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -76,11 +76,12 @@ class HashTable(object):
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, return value associated with given key
-        # TODO: Otherwise, raise error to tell user get failed
-        # Hint: raise KeyError('Key not found: {}'.format(key))
+       
+        key_index = self._bucket_index(key)
+        for pair in self.buckets[key_index].items():
+            if pair[0] == key:
+                return pair[1]
+        raise KeyError('Key not found: {}'.format(key))
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
