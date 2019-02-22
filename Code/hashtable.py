1ce915806689a45b41a2a2cdb484181d3adbe947
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-21 18:43:08 -0800

    implemented length()

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -56,8 +56,11 @@ class HashTable(object):
     def length(self):
         """Return the number of key-value entries by traversing its buckets.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all buckets
-        # TODO: Count number of key-value entries in each bucket
+    
+        entries = 0
+        for bucket in self.buckets:
+            entries += bucket.length()
+        return entries
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
